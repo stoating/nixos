2b6dbc4c4cabcd632d01769574e2702976ab5886
stoating <zack.slade@gmail.com>
2026-04-06 13:06:17 +0200

    add some default keybindings

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,7 +1,14 @@
-{
+{ inputs, ... }: {
   config = {
     systems = [
       "x86_64-linux"
     ];
+
+    perSystem = { system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
   };
 }
--- modules/programs/niri/niri.nix
+++ modules/programs/niri/niri.nix
@@ -55,9 +55,38 @@
         '';
 
         binds = {
-          "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-          "Mod+Q".close-window = _: {};
-          "Mod+S".spawn-sh = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
+          # --- Compositor ---
+          "Mod+F1".show-hotkey-overlay = _: {};
+          "Mod+Tab".toggle-overview    = _: {};
+
+          # --- Launchers ---
+          "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+          "Mod+G".spawn-sh      = lib.getExe pkgs.google-chrome;
+          "Mod+S".spawn-sh      = "${lib.getExe self'.packages.noctalia} ipc call launcher toggle";
+          "Mod+Y".spawn-sh      = lib.getExe pkgs.youtube-music;
+          "Mod+C".spawn-sh      = lib.getExe pkgs.vscode;
+
+          # --- Windows ---
+          "Mod+Q".close-window               = _: {};
+          "Mod+F".fullscreen-window          = _: {};
+          "Mod+V".toggle-window-floating     = _: {};
+          "Mod+Shift+V".switch-focus-between-floating-and-tiling = _: {};
+
+          # --- Columns ---
+          "Mod+Left".focus-column-left   = _: {};
+          "Mod+Right".focus-column-right = _: {}; 
+          "Mod+Shift+Left".move-column-left   = _: {};
+          "Mod+Shift+Right".move-column-right = _: {};
+          "Mod+R".switch-preset-column-width     = _: {};
+          "Mod+W".maximize-column                = _: {};
+          "Mod+BracketLeft".consume-or-expel-window-left   = _: {};
+          "Mod+BracketRight".consume-or-expel-window-right = _: {};
+
+          # --- Workspaces ---
+          "Mod+Up".focus-workspace-up                    = _: {};
+          "Mod+Down".focus-workspace-down                = _: {};
+          "Mod+Shift+Up".move-column-to-workspace-up     = _: {};
+          "Mod+Shift+Down".move-column-to-workspace-down = _: {};
         };
       };
     };
