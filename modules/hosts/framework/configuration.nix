626df8dcd995d154c956fc0bb1888d65286f9f0c
stoating <zack.slade@gmail.com>
2026-04-12 23:00:04 +0200

    clean configuration

--- modules/hosts/framework/configuration.nix
+++ modules/hosts/framework/configuration.nix
@@ -6,82 +6,70 @@
       self.nixosModules.home-zack
     ];
 
-    # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    # Use latest kernel.
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-
-    boot.initrd.luks.devices."luks-e13d7e39-2fc5-4ce4-8f3c-420e2197a26b".device = "/dev/disk/by-uuid/e13d7e39-2fc5-4ce4-8f3c-420e2197a26b";
-    networking.hostName = "nixos"; # Define your hostname.
-
-    nix = {
-      settings = {
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-      };
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+      kernelPackages = pkgs.linuxPackages_latest;
+      initrd.luks.devices."luks-e13d7e39-2fc5-4ce4-8f3c-420e2197a26b".device =
+        "/dev/disk/by-uuid/e13d7e39-2fc5-4ce4-8f3c-420e2197a26b";
     };
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+    networking = {
+      hostName = "nixos";
+      networkmanager.enable = true;
+    };
 
-    # Set your time zone.
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
     time.timeZone = "Europe/Berlin";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
-
-    hardware = {
-      bluetooth = {
-        enable = true;
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS        = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT    = "de_DE.UTF-8";
+        LC_MONETARY       = "de_DE.UTF-8";
+        LC_NAME           = "de_DE.UTF-8";
+        LC_NUMERIC        = "de_DE.UTF-8";
+        LC_PAPER          = "de_DE.UTF-8";
+        LC_TELEPHONE      = "de_DE.UTF-8";
+        LC_TIME           = "de_DE.UTF-8";
       };
     };
 
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
+    hardware.bluetooth.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
-
-    # Configure keymap in X11
-    services.xserver.xkb = {
-      layout  = config.keyboard.xkb.layout;
-      variant = config.keyboard.xkb.variant;
+    services = {
+      xserver = {
+        enable = true;
+        xkb = {
+          layout  = config.keyboard.xkb.layout;
+          variant = config.keyboard.xkb.variant;
+        };
+      };
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      pulseaudio.enable = false;
+      pipewire = {
+        enable            = true;
+        alsa.enable       = true;
+        alsa.support32Bit = true;
+        pulse.enable      = true;
+      };
     };
 
-    # Configure console keymap
     console.keyMap = config.keyboard.xkb.layout;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
 
     users.users.zack = {
       isNormalUser = true;
-      description = "zack";
-      extraGroups = [ "networkmanager" "wheel" ];
+      description  = "zack";
+      extraGroups  = [ "networkmanager" "wheel" ];
     };
 
     nixpkgs.config.allowUnfree = true;
