3356c7bdc45c3e34104f06338fbbdc143f631078
stoating <zack.slade@gmail.com>
2026-04-30 20:36:59 +0200

    remove home manager control of known hosts

--- modules/home/zack/config/ssh.nix
+++ modules/home/zack/config/ssh.nix
@@ -15,9 +15,5 @@
         };
       };
     };
-
-    home.file.".ssh/known_hosts".text = ''
-      github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
-    '';
   };
 }
