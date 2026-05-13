cbaf5d2364d973647f9df72abc7d5fc0e890483c
stoating <zack.slade@gmail.com>
2026-05-13 20:55:25 +0200

    add keybindings for calva. update font

--- modules/home/zack/config/vscode.nix
+++ modules/home/zack/config/vscode.nix
@@ -4,6 +4,7 @@
     theme = self.lib.theme;
   in {
     home.packages = with pkgs; [
+      fira-code
       nixd
       nixfmt
     ];
@@ -34,6 +35,12 @@
           version   = "1.9.4";
           sha256    = "0n84jw2ljv1h9dl8cfmywmm9zz3j5lhzxhb280pip31psdf9ik4q";
         })
+        (pkgs.vscode-utils.extensionFromVscodeMarketplace {
+          publisher = "mohsen1";
+          name      = "prettify-json";
+          version   = "0.0.3";
+          sha256    = "1spj01dpfggfchwly3iyfm2ak618q2wqd90qx5ndvkj3a7x6rxwn";
+        })
         bbenoist.nix
         betterthantomorrow.calva
         davidanson.vscode-markdownlint
@@ -44,7 +51,34 @@
         jnoortheen.nix-ide
         redhat.vscode-yaml
         shardulm94.trailing-spaces
+        tamasfe.even-better-toml
         zainchen.json
+      ];
+      keybindings = [
+        {
+          key = "alt+t alt+a";
+          command = "calva.runAllTests";
+        }
+        {
+          key = "alt+i";
+          command = "calva.runCustomREPLCommand";
+          when = "calva:connected && calva:keybindingsEnabled && editorLangId == 'clojure'";
+        }
+        {
+          key = "alt+t alt+c";
+          command = "calva.runNamespaceTests";
+          when = "calva:connected && calva:keybindingsEnabled";
+        }
+        {
+          key = "ctrl+shift+alt+r";
+          command = "paredit.barfSexpForward";
+          when = "calva:keybindingsEnabled && editorTextFocus && editorLangId == 'clojure' && paredit:keyMap =~ /original|strict/";
+        }
+        {
+          key = "ctrl+shift+alt+e";
+          command = "paredit.slurpSexpForward";
+          when = "calva:keybindingsEnabled && editorTextFocus && editorLangId == 'clojure' && paredit:keyMap =~ /original|strict/";
+        }
       ];
       userSettings = {
         "workbench.colorTheme" = theme.vscode;
@@ -53,8 +87,14 @@
         "github.copilot.nextEditSuggestions.enabled" = true;
         "git.enableSmartCommit" = true;
         "git.autofetch" = true;
+        "editor.fontFamily" = "Fira Code";
+        "editor.fontWeight" = "475";
+        "editor.fontLigatures" = true;
+        "editor.indentSize" = "tabSize";
+        "editor.inlineSuggest.enabled" = true;
         "editor.renderWhitespace" = "all";
         "editor.renderControlCharacters" = true;
+        "editor.rulers" = [ 80 120 ];
         "terminal.integrated.defaultProfile.linux" = "zsh";
         "markdownlint.config" = {
           "MD022" = false;
